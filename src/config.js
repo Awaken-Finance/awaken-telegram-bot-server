6b66a295818cb908a4208ad78f9505a323fec754
harold-awaken <harold@awaken.finance>
2024-07-29 16:06:15 +0800

    feat: 🎸 tg bot server

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const NETWORK_ENV = process.env.BUILD_ENV;
+
+const configMap = {
+  mainnet: {
+    botToken: process.env.TELEGRAM_BOT_TOKEN_MAINNET,
+    marketUrl: "https://app.awaken.finance/overview",
+    tradeUrl: "https://app.awaken.finance/trading",
+    swapUrl: "https://app.awaken.finance/swap",
+  },
+  testnet: {
+    botToken: process.env.TELEGRAM_BOT_TOKEN_TESTNET,
+    marketUrl: "https://test-app.awaken.finance/overview",
+    tradeUrl: "https://test-app.awaken.finance/trading",
+    swapUrl: "https://test-app.awaken.finance/swap",
+  },
+};
+
+module.exports = configMap[NETWORK_ENV];
